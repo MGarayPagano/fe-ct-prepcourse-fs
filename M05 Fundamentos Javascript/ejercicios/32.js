204fe15a088b0059611db4b2996e9aae5bb41181
MatiGp <matutegp186@gmail.com>
2024-02-29 18:24:07 -0300

    Realiza ejercicio 32 M5T32.test

--- M05 Fundamentos Javascript/ejercicios/32.js
+++ M05 Fundamentos Javascript/ejercicios/32.js
@@ -2,6 +2,9 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Supongamos que 1 euro equivale a 1.20 dólares.
   // Tu código:
+  let tasaDeCambio = 1.20;
+  let valorEnDolares = euro * tasaDeCambio;
+  return valorEnDolares;
 }
 
 module.exports = deEuroAdolar;
