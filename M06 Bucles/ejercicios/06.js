3d2f7133e85fd0aa5088df10e469dade0bfadb86
MatiGp <matutegp186@gmail.com>
2024-03-05 19:57:34 -0300

    Realiza ejercicio 6 M6T6

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,8 +2,9 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
-  let numCadena = num.toString();
-  return numCadena.length === 3;
+  if(num > 99 && num < 999){
+    return true
+  }else return false
 }
 
 module.exports = tieneTresDigitos;
