7a27e5b84c34391057c20b10dbf041ffbff9b573
MatiGp <matutegp186@gmail.com>
2024-03-05 20:51:54 -0300

    Realiza ejercicio 19 M5T19

--- M06 Bucles/ejercicios/19.js
+++ M06 Bucles/ejercicios/19.js
@@ -2,6 +2,25 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+  let sum = 0;
+
+  for(let i = 1; i <= n;i++){
+    sum += i
+  }
+  return sum;
+
+
+
+
+
 }
+  //if (n <= 0) {
+    //return 0; 
+  //} else {
+    //return (n * (n + 1)) / 2; 
+  //}
+
+
+
 
 module.exports = sumarHastaN;
