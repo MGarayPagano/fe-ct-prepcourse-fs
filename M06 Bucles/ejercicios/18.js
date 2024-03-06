39db8b41cff5a6c5822aa84d5d23f983a19c0b79
MatiGp <matutegp186@gmail.com>
2024-03-05 21:04:46 -0300

    Realiza ejercicio 18 M6T18

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -2,7 +2,6 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
-  
   let producto = 1;
     for (let i = a; i <= b; i++) {
       if(i === 0) return 0;
