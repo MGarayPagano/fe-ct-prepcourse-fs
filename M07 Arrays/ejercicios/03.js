8e4887a8b9182279c5246f431c655fd20482a0a8
MatiGp <matutegp186@gmail.com>
2024-03-14 10:10:33 -0300

    exercise 3 finished M7T3

--- M07 Arrays/ejercicios/03.js
+++ M07 Arrays/ejercicios/03.js
@@ -2,6 +2,13 @@ function encontrarElemento(elemento, array) {
   // Busca el elemento pasado por argumento dentro del array.
   // Si no se encuentra, retorna -1.
   // Tu código:
+  array.push(elemento)
+
+  for(let i = 0; i <= array.length; i++){
+    if(array.length[i] == array.length[elemento]){
+      return -1
+    }
+  }
 }
 
 module.exports = encontrarElemento;
