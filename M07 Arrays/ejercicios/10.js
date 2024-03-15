a8a6937ac3e7856261de507087c565a055d506cf
MatiGp <matutegp186@gmail.com>
2024-03-15 09:53:16 -0300

    Exercise 10 finished M7T10

--- M07 Arrays/ejercicios/10.js
+++ M07 Arrays/ejercicios/10.js
@@ -2,6 +2,16 @@ function encontrarIndiceMayor(array) {
   // Encuentra el índice del número más grande en el array de números.
   // Devuelve el valor de este índice.
   // Tu código:
+ let numMayor = array[0];
+ let indiceMayor = 0;
+
+ for (let i = 0; i < array.length; i++) {
+  if(array[i] > numMayor){
+    numMayor = array[i]
+    indiceMayor = i
+  }
+ }
+ return indiceMayor
 }
 
 module.exports = encontrarIndiceMayor;
