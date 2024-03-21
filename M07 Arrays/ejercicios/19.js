55aadf0eacc85cb55587c545f2d7382ac551a22f
MatiGp <matutegp186@gmail.com>
2024-03-21 10:09:18 -0300

    Exercise 19 finished M7T19

--- M07 Arrays/ejercicios/19.js
+++ M07 Arrays/ejercicios/19.js
@@ -5,6 +5,19 @@ function continueStatement(num) {
   // se continúa con la siguiente iteración.
   // PISTA: Utiliza el statement 'continue'.
   // Tu código:
+  let resultado = [];
+  let suma = num;
+
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      continue;
+    }
+    suma += 2;
+    resultado.push(suma);
+  }
+
+  return resultado;
+
 }
 
 module.exports = continueStatement;
